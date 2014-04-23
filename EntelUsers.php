cc6d67e763545153d6525d0e1149fa1da2c3297d
ian mukunya <ianmukunya@gmail.com>
2014-04-23 17:26:38 +0300

    add functions into a class

--- EntelUsers.php
+++ EntelUsers.php
@@ -1,21 +1,24 @@
 <?php
-function generateUserCode($length = 10) {
-	$characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	$randomString = '';
-	for ($i = 0; $i < $length; $i++) {
-		$randomString .= $characters[rand(0, strlen($characters) - 1)];
-	}
-	return $randomString;
-}
 
-function genRandAuth() {
-	$length = 7;
-	$characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-	$randomString = '';
-	for ($i = 0; $i < $length; $i++) {
-		$randomString .= $characters[rand(0, strlen($characters) - 1)];
+class EntelUser {
+	function generateUserCode($length = 10) {
+		$characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$randomString = '';
+		for ($i = 0; $i < $length; $i++) {
+			$randomString .= $characters[rand(0, strlen($characters) - 1)];
+		}
+		return $randomString;
 	}
-	return $randomString;
-}
 
+	function genRandAuth() {
+		$length = 7;
+		$characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+		$randomString = '';
+		for ($i = 0; $i < $length; $i++) {
+			$randomString .= $characters[rand(0, strlen($characters) - 1)];
+		}
+		return $randomString;
+	}
+
+}
 ?>
