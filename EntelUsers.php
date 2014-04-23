52a2d508f79f78c38b1cfed7d350a83b07b2c0d7
ian mukunya <ianmukunya@gmail.com>
2014-04-23 17:19:44 +0300

    update EntelUsers.php

--- EntelUsers.php
+++ EntelUsers.php
@@ -1,3 +1,21 @@
 <?php
+function generateUserCode($length = 10) {
+	$characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$randomString = '';
+	for ($i = 0; $i < $length; $i++) {
+		$randomString .= $characters[rand(0, strlen($characters) - 1)];
+	}
+	return $randomString;
+}
+
+function genRandAuth() {
+	$length = 7;
+	$characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+	$randomString = '';
+	for ($i = 0; $i < $length; $i++) {
+		$randomString .= $characters[rand(0, strlen($characters) - 1)];
+	}
+	return $randomString;
+}
 
 ?>
