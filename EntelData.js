d8e3915be6aa817ec9154ac5ff49240e995ccc14
ian mukunya <ianmukunya@gmail.com>
2014-11-07 17:09:03 +0300

    addition of javascript cookies

--- EntelData.js
+++ EntelData.js
@@ -1,9 +1,9 @@
-var EntelData={
-	
-	"fetchData":function($data,$method,$server,$datatype) {
+var EntelData = {
+
+	"fetchData" : function($data, $method, $server, $datatype) {
 		$.ajax({
 			cache : false,
-			url:$scipt,
+			url : $scipt,
 			dataType : $datatype,
 			data : Sdata,
 			type : $method,
@@ -11,12 +11,44 @@ var EntelData={
 
 			},
 			complete : function() {
-				
+
 			},
-			success:function(Sdata){
+			success : function(Sdata) {
 				return Sdata;
 			}
 		});
 	}
-
 };
+
+var SetPrefs = {
+	getCookie : function(name) {
+		var start = document.cookie.indexOf(name + "=");
+		var len = start + name.length + 1;
+		if ((!start ) && (name != document.cookie.substring(0, name.length) )) {
+			return null;
+		}
+		if (start == -1)
+			return null;
+		var end = document.cookie.indexOf(';', len);
+		if (end == -1)
+			end = document.cookie.length;
+		return unescape(document.cookie.substring(len, end));
+	},
+
+	//set cookie
+	setCookie : function(name, value, expires, path, domain, secure) {
+		var today = new Date();
+		today.setTime(today.getTime());
+		if (expires) {
+			expires = expires * 1000 * 60 * 60 * 24;
+		}
+		var expires_date = new Date(today.getTime() + (expires));
+		document.cookie = name + '=' + escape(value) + ((expires ) ? ';expires=' + expires_date.toGMTString() : '' ) + //expires.toGMTString()
+		((path ) ? ';path=' + path : '' ) + ((domain ) ? ';domain=' + domain : '' ) + ((secure ) ? ';secure' : '' );
+	},
+	//delete cookie
+	deleteCookie : function(name, path, domain) {
+		if (getCookie(name))
+			document.cookie = name + '=' + ((path ) ? ';path=' + path : '') + ((domain ) ? ';domain=' + domain : '' ) + ';expires=Thu, 01-Jan-1970 00:00:01 GMT';
+	}
+}; 
