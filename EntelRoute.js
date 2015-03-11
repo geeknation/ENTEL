2f3f05fbe4104f6040c6203fbcfec9b248a8bf5e
ian mukunya <ianmukunya@gmail.com>
2015-03-11 17:09:43 +0300

    routing modified

--- EntelRoute.js
+++ EntelRoute.js
@@ -12,7 +12,27 @@ if(location.href.match(pattern)){
 this();
 }
 });
+},
+
+parseUrl:function(url){
+	
+	// You want to parse this address into parts:
+	var url = 'http://www.patikana.co.ke/createaccount.php';
+	// The trick; create a new link with the url as its href:
+	var URLparts=[];
+	var a = $('<a>',{ href: url });
+	URLparts.push('Host name: ' + a.prop('hostname'));
+	URLparts.push('Path: ' + a.prop('pathname'));
+	URLparts.push('Query: ' + a.prop('search'));
+	URLparts.push('Protocol: ' + a.prop('protocol'));
+	URLparts.push('Hash: ' + a.prop('hash'));
+	console.log(URLparts);
+	
+	
 }
+
+
+
 }
 // Will execute only on this page:
 route.add('page.html', function(){
@@ -21,8 +41,10 @@ route.add('page.html', function(){
 route.add('page.html', function(){
 	
 });
+
 // You can even use regex-es:
 route.add('.*.html', function(){
 	alert('This is using a regex!');
 });
+
 route.run();
