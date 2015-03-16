4b1d744dfcfa8aef9246bd80f529702fea8785c2
ian mukunya <ianmukunya@gmail.com>
2015-03-16 11:29:40 +0300

    data query layer addition

---
+++ data/read/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IMukunya
+ * Date: 3/16/2015
+ * Time: 10:23 AM
+ */
+include "Data.php";
+$query=$_GET['query'];
+
+if($query==""){
+    echo "error:please include a query your request";
+}else{
+
+}
